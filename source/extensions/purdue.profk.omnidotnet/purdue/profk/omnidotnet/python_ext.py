eacd31b00250bfba3be587a16bf9744d1a90cd35
professorK <jeffpk@gmail.com>
2024-04-12 15:00:55 -0400

    Test C# call worked!

--- source/extensions/purdue.profk.omnidotnet/purdue/profk/omnidotnet/python_ext.py
+++ source/extensions/purdue.profk.omnidotnet/purdue/profk/omnidotnet/python_ext.py
@@ -3,6 +3,7 @@ from pythonnet import load
 load("coreclr")
 
 import clr
+from System import Console
 
 
 # Functions and vars are available to other extension as usual in python: `example.python_ext.some_public_function(x)`
@@ -14,11 +15,12 @@ def some_public_function(x: int):
 # Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
 # instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
 # on_shutdown() is called.
-class HelloPythonExtension(omni.ext.IExt):
+class OmniDotNet(omni.ext.IExt):
     # ext_id is current extension id. It can be used with extension manager to query additional information, like where
     # this extension is located on filesystem.
     def on_startup(self, ext_id):
-        print("[purdue.profk.omnidotnet] HelloPythonExtension startup")
+        Console.WriteLine("!!!Writing from DotNet!!!")
+
 
     def on_shutdown(self):
         print("[purdue.profk.omnidotnet] HelloPythonExtension shutdown")
